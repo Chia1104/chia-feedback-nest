daf48096b3cfcda556a2ece6bfc7bf116a9130a7
Chia1104 <38397958+Chia1104@users.noreply.github.com>
2022-04-06 00:08:32 +0800

    connect to mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
-import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { ReposController } from './repos/controllers/repos.controller';
+import { UsersController } from './users/controllers/users.controller';
+import { FeedbacksController } from './feedbacks/controllers/feedbacks.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_CONNECTION_URI, {
+      dbName: process.env.MONGODB_DATABASE,
+    }),
+  ],
+  controllers: [
+    AppController,
+    ReposController,
+    UsersController,
+    FeedbacksController,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
