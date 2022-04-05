c87b8fff4d1105fa8bb98a0a6570c18188d91c5f
Chia1104 <38397958+Chia1104@users.noreply.github.com>
2022-04-06 01:12:26 +0800

    add API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle("Chia's Feedback")
+    .setDescription("Chia's feedback REST API")
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/documentation', app, document);
   await app.listen(3000);
 }
 bootstrap().then(() => console.log('Server running on port 3000'));
